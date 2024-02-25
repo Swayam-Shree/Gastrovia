ec339ce3ef00c1e0c1e2ca2365bf4b3161f418e7
Swayam-Shree <61933177+Swayam-Shree@users.noreply.github.com>
2024-02-25 16:59:26 +0530

    removed custom text inputs due to android virtual keyboard issue

--- src/login.js
+++ src/login.js
@@ -1,18 +1,19 @@
-import { Button, TextBox } from './ui.js';
+import { Button } from './ui.js';
 import { nextState } from './util.js';
 
-let playButton, nameBox, passwordBox;
-let showInvalidFieldPrompt = false;
+let playButton;
+// let nameBox, passwordBox;
+// let showInvalidFieldPrompt = false;
 
 export function loginSetup(p) {
-	nameBox = new TextBox(p, p.width / 2, p.height / 2 - 150, 200, 50, "Name...", 20, 16);
-	passwordBox = new TextBox(p, p.width / 2, p.height / 2 - 75, 200, 50, "Password...", 20, 16, true);
-	playButton = new Button(p, p.width / 2, p.height / 2 + 150, 100, 50, "Play", 20, () => {
-		if (nameBox.text && passwordBox.text) {
+	// nameBox = new TextBox(p, p.width / 2, p.height / 2 - 150, 200, 50, "Name...", 20, 16);
+	// passwordBox = new TextBox(p, p.width / 2, p.height / 2 - 75, 200, 50, "Password...", 20, 16, true);
+	playButton = new Button(p, p.width / 2, p.height / 2, 100, 50, "Play", 20, () => {
+		// if (nameBox.text && passwordBox.text) {
 			nextState();
-		} else {
-			showInvalidFieldPrompt = true;
-		}
+		// } else {
+		// 	showInvalidFieldPrompt = true;
+		// }
 	});
 }
 
@@ -20,24 +21,24 @@ export function loginDraw(p) {
 	p.background(0);
 
 	playButton.run();
-	nameBox.run();
-	passwordBox.run();
+	// nameBox.run();
+	// passwordBox.run();
 
-	if (showInvalidFieldPrompt) {
-		p.fill(255, 0, 0);
-		p.textSize(20);
-		p.textAlign(p.CENTER, p.CENTER);
-		p.text("Please fill in all fields", p.width / 2, p.height / 2);
-	}
+	// if (showInvalidFieldPrompt) {
+	// 	p.fill(255, 0, 0);
+	// 	p.textSize(20);
+	// 	p.textAlign(p.CENTER, p.CENTER);
+	// 	p.text("Please fill in all fields", p.width / 2, p.height / 2);
+	// }
 }
 
 export function loginKeyPressed(p) {
-	nameBox.keyPressed();
-	passwordBox.keyPressed();
+	// nameBox.keyPressed();
+	// passwordBox.keyPressed();
 }
 
 export function loginMousePressed(p) {
 	playButton.mousePressed();
-	nameBox.mousePressed();
-	passwordBox.mousePressed();
+	// nameBox.mousePressed();
+	// passwordBox.mousePressed();
 }
--- src/ui.js
+++ src/ui.js
@@ -31,62 +31,62 @@ export class Button {
 	}
 }
 
-export class TextBox {
-	static leftPadding = 15;
+// export class TextBox {
+// 	static leftPadding = 15;
 
-	constructor(p, x, y, w, h, defaultText, textSize, maxLength, hideText=false) {
-		this.p = p;
-		this.position = p.createVector(x, y);
-		this.shape = p.createVector(w, h);
-		this.defaultText = defaultText;
-		this.textSize = textSize;
-		this.selected = false;
-		this.text = "";
-		this.hideText = hideText;
-		this.maxLength = maxLength;
-	}
+// 	constructor(p, x, y, w, h, defaultText, textSize, maxLength, hideText=false) {
+// 		this.p = p;
+// 		this.position = p.createVector(x, y);
+// 		this.shape = p.createVector(w, h);
+// 		this.defaultText = defaultText;
+// 		this.textSize = textSize;
+// 		this.selected = false;
+// 		this.text = "";
+// 		this.hideText = hideText;
+// 		this.maxLength = maxLength;
+// 	}
 	
-	run(bgColor=this.p.color(255), hoverBgColor=this.p.color(170), selectedBgColor=this.p.color(210), defaultTextColor=this.p.color(100), textColor=this.p.color(0)) {
-		if (this.selected){
-			this.p.fill(selectedBgColor);
-		} else if (pointInRect(this.p.createVector(this.p.mouseX, this.p.mouseY), this.position, this.shape)) {
-			this.p.fill(hoverBgColor);
-		} else {
-			this.p.fill(bgColor);
-		}
-		this.p.rect(this.position.x, this.position.y, this.shape.x, this.shape.y);
-		this.p.textSize(this.textSize);
-		this.p.textAlign(this.p.LEFT, this.p.CENTER);
+// 	run(bgColor=this.p.color(255), hoverBgColor=this.p.color(170), selectedBgColor=this.p.color(210), defaultTextColor=this.p.color(100), textColor=this.p.color(0)) {
+// 		if (this.selected){
+// 			this.p.fill(selectedBgColor);
+// 		} else if (pointInRect(this.p.createVector(this.p.mouseX, this.p.mouseY), this.position, this.shape)) {
+// 			this.p.fill(hoverBgColor);
+// 		} else {
+// 			this.p.fill(bgColor);
+// 		}
+// 		this.p.rect(this.position.x, this.position.y, this.shape.x, this.shape.y);
+// 		this.p.textSize(this.textSize);
+// 		this.p.textAlign(this.p.LEFT, this.p.CENTER);
 	
-		if (this.text === "" && !this.selected) {
-			this.p.fill(defaultTextColor);
-			this.p.text(this.defaultText, this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
-		} else {
-			this.p.fill(textColor);
-			let displayText = this.hideText ? "*".repeat(this.text.length) : this.text;
-			if (this.selected) {
-				if (this.p.round(globals.frameCount/30) % 2) {
-					this.p.text(displayText, this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
-				} else {
-					this.p.text(displayText + "|", this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
-				}
-			} else {
-				this.p.text(displayText, this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
-			}
-		}
-	}
+// 		if (this.text === "" && !this.selected) {
+// 			this.p.fill(defaultTextColor);
+// 			this.p.text(this.defaultText, this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
+// 		} else {
+// 			this.p.fill(textColor);
+// 			let displayText = this.hideText ? "*".repeat(this.text.length) : this.text;
+// 			if (this.selected) {
+// 				if (this.p.round(globals.frameCount/30) % 2) {
+// 					this.p.text(displayText, this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
+// 				} else {
+// 					this.p.text(displayText + "|", this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
+// 				}
+// 			} else {
+// 				this.p.text(displayText, this.position.x + TextBox.leftPadding, this.position.y, this.shape.x, this.shape.y);
+// 			}
+// 		}
+// 	}
 
-	keyPressed() {
-		if (this.selected) {
-			if (this.p.keyCode === 8) {
-				this.text = this.text.slice(0, -1);
-			} else if (this.p.keyCode >= 32 && this.p.keyCode <= 126 && this.text.length < this.maxLength) {
-				this.text += this.p.key;
-			}
-		}
-	}
+// 	keyPressed() {
+// 		if (this.selected) {
+// 			if (this.p.keyCode === 8) {
+// 				this.text = this.text.slice(0, -1);
+// 			} else if (this.p.keyCode >= 32 && this.p.keyCode <= 126 && this.text.length < this.maxLength) {
+// 				this.text += this.p.key;
+// 			}
+// 		}
+// 	}
 
-	mousePressed() {
-		this.selected = pointInRect(this.p.createVector(this.p.mouseX, this.p.mouseY), this.position, this.shape);
-	}
-}
+// 	mousePressed() {
+// 		this.selected = pointInRect(this.p.createVector(this.p.mouseX, this.p.mouseY), this.position, this.shape);
+// 	}
+// }
